9552a0d0b8b5a4fe7000833ee88aed4c2c83ef21
seeu324 <55227118+seeu324@users.noreply.github.com>
2019-11-07 14:42:18 +0800

    1107期中

---
+++ UnityProject/Assets/C/NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public AudioMixer bb;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void bgm (float bmg)
+    {
+        bb.SetFloat("SFX", bmg);
+
+    }
+}
